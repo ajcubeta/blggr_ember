5fe6ba39c0c8fcd67cdea93293817f31b3ca835b
ajcubeta <ajcubeta@gmail.com>
2014-05-26 18:35:37 +0800

    Generate home controller and devise for initial setup

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    @title = "Welcome to Blggr using Ember JS"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlggrEmber::Application.config.secret_token = 'eb06223ec93c7f580529c5c10f32f25a5f59ec6e2117fe008d53dd8f84807610bc01ee3dd75d8a2192d3a3d63d7f457a5fd113b893822a8c15229d41163fd51b'
